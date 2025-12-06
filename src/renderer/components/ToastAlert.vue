cf329b8ae1450b0ed63199e50a0470c5488a1220
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2025-12-06 16:10:27 +0100

    fix toast

--- src/renderer/components/ToastAlert.vue
+++ src/renderer/components/ToastAlert.vue
@@ -36,7 +36,7 @@
     <ToastRoot
       :key="new Date().getTime()"
       v-model:open="open"
-      class="rounded-lg shadow-sm border p-4 grid [grid-template-areas:_'title_action'_'description_action'] grid-cols-[1fr_max-content] gap-x-4 gap-y-2 items-start data-[state=open]:animate-slideIn data-[state=closed]:animate-hide data-[swipe=move]:translate-x-[var(--reka-toast-swipe-move-x)] data-[swipe=cancel]:translate-x-0 data-[swipe=cancel]:transition-[transform_200ms_ease-out] data-[swipe=end]:animate-swipeOut"
+      class="rounded-lg shadow-sm border p-4"
       :style="{
         backgroundColor: settingsStore.colors.backgroundLight,
         borderColor: settingsStore.colors.border,
@@ -64,7 +64,7 @@
       </ToastAction>
     </ToastRoot>
     <ToastViewport
-      class="fixed bottom-0 right-0 flex flex-col p-6 gap-3 w-auto max-w-[400px] m-0 list-none z-[2147483647] outline-none"
+      class="fixed bottom-0 right-0 flex flex-col py-8 px-3 gap-3 w-auto max-w-[400px] m-0 list-none z-[2147483647] outline-none"
     />
   </ToastProvider>
 </template>
